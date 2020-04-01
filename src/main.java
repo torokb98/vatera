923c14d01d28c351f0f4051bb4e140d6331071bf
torokb <torokb98@gmail.com>
2020-04-01 18:38:54 +0200

    git commit

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello világ!");
         System.out.println("Működik a rendszer!");
+        System.out.println("mindenki boldog");
         int x = 5;
         double y = 2.5;
         System.out.println(x*y);
