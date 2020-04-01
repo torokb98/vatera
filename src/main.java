ea139148c1d1047ccc0d1b9658634c0cac636b52
weberkristof <weberkristof@gmail.com>
2020-04-01 19:12:59 +0200

    git commit
    Signed-off-by: weberkristof <weberkristof@gmail.com>

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ class Hello {
         System.out.println("Hello világ!");
         System.out.println("Működik a rendszer!");
         System.out.println("mindenki boldog");
+        System.out.println("intelliJ is működik");
         int x = 5;
         double y = 2.5;
         System.out.println(x*y);
